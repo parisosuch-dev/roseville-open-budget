8bf1854ad2e04ca135464cf6d9acf116d8ba922d
paris osuch <paris.osuch@gmail.com>
2025-02-13 19:41:19 -0800

    add title

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import { Card } from "@tremor/react";
 
 export default async function Home() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
-      <Card className="w-5/6 sm:w-1/3">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-medium font-mono">
+        Roseville Open Budget 🌹
+      </h1>
+      <Card className="mt-2 sm:mt-6 w-5/6 sm:w-1/3">
         <h2 className="text-lg sm:text-2xl font-medium font-mono">
-          Budget vs Revenue By Year (in millions)
+          Expense vs Revenue By Year (in millions)
         </h2>
         <BudgetRevenueLineChart />
       </Card>
