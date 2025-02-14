1be813a279b5d5b587ad642d6d82715b95e030d3
paris osuch <paris.osuch@gmail.com>
2025-02-13 19:17:50 -0800

    format for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Card } from "@tremor/react";
 export default async function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
-      <Card className="w-1/3">
-        <h2 className="text-2xl font-medium font-mono">
+      <Card className="w-5/6 sm:w-1/3">
+        <h2 className="text-lg sm:text-2xl font-medium font-mono">
           Budget vs Revenue By Year (in millions)
         </h2>
         <BudgetRevenueLineChart />
