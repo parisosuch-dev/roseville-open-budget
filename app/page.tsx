4e91ec79b5c76ec1f5c34c105e39416f81d5a067
paris osuch <paris.osuch@gmail.com>
2025-02-13 19:58:33 -0800

    more styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { Card } from "@tremor/react";
 export default async function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-medium font-mono">
+      <h1 className="text-2xl sm:text-4xl font-medium font-mono">
         Roseville Open Budget 🌹
       </h1>
       <Card className="mt-2 sm:mt-6 w-5/6 sm:w-1/3">
-        <h2 className="text-lg sm:text-2xl font-medium font-mono">
+        <h2 className="text-lg sm:text-2xl text-center font-medium font-mono">
           Expense vs Revenue By Year (in millions)
         </h2>
         <BudgetRevenueLineChart />
