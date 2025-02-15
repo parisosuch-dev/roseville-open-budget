0da3f75300e83b1b2b4117de60d2cc5c1130e960
paris osuch <paris.osuch@gmail.com>
2025-02-14 19:31:18 -0800

    feat: get expense by fundCategory by year

--- lib/neon/expense.ts
+++ lib/neon/expense.ts
@@ -7,10 +7,29 @@ export const expenseByYear = async () => {
     const result = await db
       .select({
         fiscalYear: expense.fiscal_year,
-        totalExpenses: sql<number>`SUM(${expense.adopted_budget})`, // Use `sql` helper
+        totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
       })
       .from(expense)
       .groupBy(expense.fiscal_year)
+      .orderBy(expense.fiscal_year);
+
+    return result;
+  } catch (error) {
+    console.error("Error fetching expense by year:", error);
+    throw error;
+  }
+};
+
+export const expenseByFundCategory = async () => {
+  try {
+    const result = await db
+      .select({
+        fundCategory: expense.fund_category_fund_level,
+        fiscalYear: expense.fiscal_year,
+        totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
+      })
+      .from(expense)
+      .groupBy(expense.fund_category_fund_level, expense.fiscal_year)
       .orderBy(expense.fiscal_year);
 
     return result;
