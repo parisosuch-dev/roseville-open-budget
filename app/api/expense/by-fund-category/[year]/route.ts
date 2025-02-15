ad79b682db57b999f2308709b7cdadab6fc6cdcb
paris osuch <paris.osuch@gmail.com>
2025-02-14 20:35:35 -0800

    bugfix: params should be awaited

--- app/api/expense/by-fund-category/[year]/route.ts
+++ app/api/expense/by-fund-category/[year]/route.ts
@@ -5,7 +5,7 @@ export async function GET(
   request: Request,
   { params }: { params: { year: number } }
 ) {
-  const { year } = params;
+  const { year } = await params;
 
   const data = await expenseByFundCategory(year);
 
