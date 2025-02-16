7dd9a6884f08fef6c5e83f7decc2e023440d7b27
parisosuch-dev <parisosuch.dev@gmail.com>
2025-02-16 09:23:49 -0800

    change screen size

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,23 @@ import { Card } from "@tremor/react";
 export default async function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <h1 className="text-2xl sm:text-4xl text-center font-medium font-mono">
-        Roseville Open Budget 🌹
-      </h1>
-      <Card className="mt-6 sm:mt-12 w-5/6 sm:w-1/3">
-        <h2 className="text-lg sm:text-2xl text-center font-medium font-mono">
-          Expense vs Revenue By Year (in millions)
-        </h2>
-        <BudgetRevenueLineChart />
-      </Card>
-      <Card className="mt-6 sm:mt-12 w-5/6 sm:w-1/3">
-        <h2 className="text-lg sm:text-2xl text-center font-medium font-mono">
-          Spending by Top Level Funding Category
-        </h2>
-        <ExpensesByFundCategoryChart />
-      </Card>
+      <div className="w-5/6 sm:w-2/3 2xl:w-1/3">
+        <h1 className="text-2xl sm:text-4xl text-center font-medium font-mono">
+          Roseville Open Budget 🌹
+        </h1>
+        <Card className="mt-6 sm:mt-12">
+          <h2 className="text-lg sm:text-2xl text-center font-medium font-mono">
+            Expense vs Revenue By Year (in millions)
+          </h2>
+          <BudgetRevenueLineChart />
+        </Card>
+        <Card className="mt-6 sm:mt-12">
+          <h2 className="text-lg sm:text-2xl text-center font-medium font-mono">
+            Spending by Top Level Funding Category
+          </h2>
+          <ExpensesByFundCategoryChart />
+        </Card>
+      </div>
     </div>
   );
 }
