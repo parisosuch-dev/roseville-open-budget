0adba9503defba0ccb03d36cc3ff9952f62b5cd9
paris osuch <paris.osuch@gmail.com>
2025-02-17 18:08:38 -0800

    feat: add function to get years from expense data

---
+++ lib/neon/meta.ts
@@ -0,0 +1,26 @@
+import db from "./db";
+import { sql, eq } from "drizzle-orm";
+import { expense } from "./schema";
+
+export const getExpenseYears = async () => {
+  try {
+    const result = await db
+      .select({
+        fiscalYear: expense.fiscal_year,
+      })
+      .from(expense)
+      .groupBy(expense.fiscal_year)
+      .orderBy(expense.fiscal_year);
+
+    let years = [];
+
+    for (let r of result) {
+      years.push(r.fiscalYear);
+    }
+
+    return years;
+  } catch (error) {
+    console.error("Error fetching expense by year:", error);
+    throw error;
+  }
+};
