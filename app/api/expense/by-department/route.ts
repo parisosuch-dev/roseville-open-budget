dc808f31b07567b8ba4cc7988722e434e87ca8d1
paris osuch <paris.osuch@gmail.com>
2025-03-07 21:02:16 -0800

    add api route for by deparment

---
+++ app/api/expense/by-department/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { expenseByDepartment } from "@/lib/neon/expense";
+
+export async function GET() {
+  try {
+    const data = await expenseByDepartment();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "An unknown error occurred." },
+      { status: 500 }
+    );
+  }
+}
