ab679174b6dfcb0f3082bea54096135511b7a0e1
parisosuch-dev <parisosuch.dev@gmail.com>
2025-03-09 12:06:53 -0700

    working endpoint but there is a bug with the query

--- app/api/expense/route.ts
+++ app/api/expense/route.ts
@@ -1,10 +1,27 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import db from "@/lib/neon/db";
 import { expense } from "@/lib/neon/schema";
+import { eq } from "drizzle-orm";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
-    const data = await db.select().from(expense);
+    // get query params
+    const params = req.nextUrl.searchParams;
+
+    const year = params.get("year") as number | null;
+    const department = params.get("department");
+
+    const query = db.select().from(expense);
+
+    if (year) {
+      query.where(eq(expense.fiscal_year, year));
+    }
+    if (department) {
+      query.where(eq(expense.department_cost_center_level, department));
+    }
+
+    const data = await query;
+
     return NextResponse.json(data);
   } catch (error) {
     if (error instanceof Error) {
