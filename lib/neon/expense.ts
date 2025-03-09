b55bf34d923a54fb70fdb4e54122bc808a27f6a9
paris osuch <paris.osuch@gmail.com>
2025-03-09 15:17:18 -0700

    add function to get cost center sum

--- lib/neon/expense.ts
+++ lib/neon/expense.ts
@@ -136,3 +136,69 @@ export const expenseByDepartment = async (
     throw error;
   }
 };
+
+export const expenseByCostCenter = async (
+  year?: number,
+  department?: string
+) => {
+  try {
+    if (year && department) {
+      const result = await db
+        .select({
+          fundCategory: expense.fund_category_fund_level,
+          fiscalYear: expense.fiscal_year,
+          totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
+        })
+        .from(expense)
+        .where(
+          and(
+            eq(expense.fiscal_year, year),
+            eq(expense.department_cost_center_level, department)
+          )
+        )
+        .groupBy(expense.cost_center_cost_center_level, expense.fiscal_year)
+        .orderBy(expense.fiscal_year);
+
+      return result;
+    } else if (year && !department) {
+      const result = await db
+        .select({
+          fundCategory: expense.fund_category_fund_level,
+          fiscalYear: expense.fiscal_year,
+          totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
+        })
+        .from(expense)
+        .where(eq(expense.fiscal_year, year))
+        .groupBy(expense.cost_center_cost_center_level, expense.fiscal_year)
+        .orderBy(expense.fiscal_year);
+
+      return result;
+    } else if (!year && department) {
+      const result = await db
+        .select({
+          fundCategory: expense.fund_category_fund_level,
+          fiscalYear: expense.fiscal_year,
+          totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
+        })
+        .from(expense)
+        .where(eq(expense.department_cost_center_level, department))
+        .groupBy(expense.cost_center_cost_center_level, expense.fiscal_year)
+        .orderBy(expense.fiscal_year);
+
+      return result;
+    }
+    const result = await db
+      .select({
+        fundCategory: expense.fund_category_fund_level,
+        fiscalYear: expense.fiscal_year,
+        totalExpenses: sql<number>`SUM(${expense.adopted_budget})`,
+      })
+      .from(expense)
+      .groupBy(expense.fund_category_fund_level, expense.fiscal_year)
+      .orderBy(expense.fiscal_year);
+    return result;
+  } catch (error) {
+    console.error("Error fetching expense by year:", error);
+    throw error;
+  }
+};
